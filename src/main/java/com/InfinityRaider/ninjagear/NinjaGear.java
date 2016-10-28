7601f6724f374b56285cd89fbb70c183afc007c7
InfinityRaider <the_xaf@hotmail.com>
2016-10-28 22:06:35 +0200

    Update to newest InfinityLib

--- src/main/java/com/InfinityRaider/ninjagear/NinjaGear.java
+++ src/main/java/com/InfinityRaider/ninjagear/NinjaGear.java
@@ -9,7 +9,6 @@ import com.infinityraider.ninjagear.reference.Reference;
 import com.infinityraider.ninjagear.registry.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.*;
 
 @Mod(
         modid = Reference.MOD_ID,
@@ -54,53 +53,5 @@ public class NinjaGear extends InfinityMod {
         wrapper.registerMessage(MessageInvisibility.class);
         wrapper.registerMessage(MessageUpdateGadgetRenderMaskClient.class);
         wrapper.registerMessage(MessageUpdateGadgetRenderMaskServer.class);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void onPreInit(FMLPreInitializationEvent event) {
-        super.preInit(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void onInit(FMLInitializationEvent event) {
-        super.init(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void onPostInit(FMLPostInitializationEvent event) {
-        super.postInit(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-        super.onServerAboutToStart(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void serverStart(FMLServerStartingEvent event) {
-        super.onServerStarting(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void serverStarted(FMLServerStartedEvent event) {
-        super.onServerStarted(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void serverStopping(FMLServerStoppingEvent event) {
-        super.onServerStopping(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void serverStopped(FMLServerStoppedEvent event) {
-        super.onServerStopped(event);
     }
 }
