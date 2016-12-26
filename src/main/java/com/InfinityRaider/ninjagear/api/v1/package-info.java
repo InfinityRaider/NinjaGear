e56df261bd04c0bacca558d36c9e2987f0c9ee35
InfinityRaider <the_xaf@hotmail.com>
2016-12-26 12:41:39 +0100

    update api package-info.java

--- src/main/java/com/InfinityRaider/ninjagear/api/v1/package-info.java
+++ src/main/java/com/InfinityRaider/ninjagear/api/v1/package-info.java
@@ -1,4 +1,4 @@
 @API(owner = "NinjaGear", provides = "NinjaGearAPI", apiVersion = "1.0")
-package com.infinityraider.ninjagear.api.v0;
+package com.infinityraider.ninjagear.api.v1;
 
 import net.minecraftforge.fml.common.API;
